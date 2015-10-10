914bf68779dedf20513bafcd6da52423cbceda00
Justin Barber <barber.justin@gmail.com>
2015-10-10 18:09:52 -0400

    using list comprehension

--- erlang/rna-transcription/rna_transcription.erl
+++ erlang/rna-transcription/rna_transcription.erl
@@ -1,12 +1,9 @@
 -module(rna_transcription).
 -export([to_rna/1, dna_rna_map/1]).
 
-dna_rna_map($G) -> "C";
-dna_rna_map($C) -> "G";
-dna_rna_map($T) -> "A";
-dna_rna_map($A) -> "U";
-dna_rna_map(_)  -> "".
+dna_rna_map($G) -> $C;
+dna_rna_map($C) -> $G;
+dna_rna_map($T) -> $A;
+dna_rna_map($A) -> $U.
 
-to_rna([])                  -> "";
-to_rna([Nucleotide|Strand]) ->
-  lists:concat([dna_rna_map(Nucleotide), to_rna(Strand)]).
+to_rna(Strand) -> [dna_rna_map(Nucleotide) || Nucleotide <- Strand].
