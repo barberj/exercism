378a06a27eaa71ede373474dbaf2e45fe2b6ea11
Justin Barber <barber.justin@gmail.com>
2015-10-12 22:52:14 -0400

    use macros

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -1,14 +1,13 @@
 -module(bob).
 -export([response_for/1]).
 
-response_for([]) -> "Fine. Be that way!";
-response_for(Phrase) ->
-  Question = "\\?$",
-  NotShouting = "[[:lower:]]",
-  Shouting = "[[:upper:]]",
-  Silence = "^[\\p{Xps}]*$",
+-define(QUESTION, "\\?$").
+-define(NOTSHOUTING, "[[:lower:]]").
+-define(SHOUTING, "[[:upper:]]").
+-define(SILENCE, "^[\\p{Xps}]*$").
 
-  case {re:run(Phrase, Question, [ucp]), re:run(Phrase, NotShouting, [ucp]), re:run(Phrase, Shouting, [ucp]), re:run(Phrase, Silence, [ucp]) } of
+response_for(Phrase) ->
+  case {re:run(Phrase, ?QUESTION, [ucp]), re:run(Phrase, ?NOTSHOUTING, [ucp]), re:run(Phrase, ?SHOUTING, [ucp]), re:run(Phrase, ?SILENCE, [ucp]) } of
     {_, nomatch, {_, _}, nomatch} -> "Whoa, chill out!";
     {{_, _}, _, _, nomatch} -> "Sure.";
     {nomatch, nomatch, nomatch, {_, _}} -> "Fine. Be that way!";
