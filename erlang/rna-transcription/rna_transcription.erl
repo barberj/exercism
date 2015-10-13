a1e150073ade93654f1dee0de12c8369de9a4a00
Justin Barber <barber.justin@gmail.com>
2015-10-12 22:05:01 -0400

    dry up rna_transcription

--- erlang/rna-transcription/rna_transcription.erl
+++ erlang/rna-transcription/rna_transcription.erl
@@ -1,9 +1,9 @@
 -module(rna_transcription).
--export([to_rna/1, dna_rna_map/1]).
+-export([to_rna/1]).
 
-dna_rna_map($G) -> $C;
-dna_rna_map($C) -> $G;
-dna_rna_map($T) -> $A;
-dna_rna_map($A) -> $U.
+to_rna($A)      -> $U;
+to_rna($C)      -> $G;
+to_rna($G)      -> $C;
+to_rna($T)      -> $A;
+to_rna(Strand)  -> lists:map(fun to_rna/1, Strand).
 
-to_rna(Strand) -> [dna_rna_map(Nucleotide) || Nucleotide <- Strand].
