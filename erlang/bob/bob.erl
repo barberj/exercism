f956731aaabd4443b8fca3f037044e8e4bb0ce53
Justin Barber <barber.justin@gmail.com>
2015-10-12 22:26:40 -0400

    regex attempt

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -2,4 +2,12 @@
 -export([response_for/1]).
 
 response_for([]) -> "Fine. Be that way!";
-response_for(_) -> "Whatever.".
+response_for(Conversation) ->
+  case re:run(Conversation, "(.+!$)|i([A-Z]{2,})") of
+    nomatch ->
+      case re:run(Conversation, ".*?.*") of
+        {match, _} -> "Sure.";
+        nomatch ->  "Whatever."
+      end;
+    {match, _} -> "Whoa, chill out!"
+  end.
