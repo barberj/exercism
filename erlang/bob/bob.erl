9d911b550024e2efb952f04e14162be429b84548
Justin Barber <barber.justin@gmail.com>
2015-10-12 22:45:26 -0400

    thanks workmad3

--- erlang/bob/bob.erl
+++ erlang/bob/bob.erl
@@ -2,12 +2,15 @@
 -export([response_for/1]).
 
 response_for([]) -> "Fine. Be that way!";
-response_for(Conversation) ->
-  case re:run(Conversation, "(.+!$)|i([A-Z]{2,})") of
-    nomatch ->
-      case re:run(Conversation, ".*?.*") of
-        {match, _} -> "Sure.";
-        nomatch ->  "Whatever."
-      end;
-    {match, _} -> "Whoa, chill out!"
+response_for(Phrase) ->
+  Question = "\\?$",
+  NotShouting = "[[:lower:]]",
+  Shouting = "[[:upper:]]",
+  Silence = "^[\\p{Xps}]*$",
+
+  case {re:run(Phrase, Question, [ucp]), re:run(Phrase, NotShouting, [ucp]), re:run(Phrase, Shouting, [ucp]), re:run(Phrase, Silence, [ucp]) } of
+    {_, nomatch, {_, _}, nomatch} -> "Whoa, chill out!";
+    {{_, _}, _, _, nomatch} -> "Sure.";
+    {nomatch, nomatch, nomatch, {_, _}} -> "Fine. Be that way!";
+    _ -> "Whatever."
   end.
