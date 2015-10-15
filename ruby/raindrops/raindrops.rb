be458212f542d4a388738e14a205b797666ce5b0
Justin Barber <barber.justin@gmail.com>
2015-10-14 20:03:22 -0400

    raindrops

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,29 +1,18 @@
-require 'prime'
-
 class Raindrops
   def self.is_factor?(number, prime)
-    number % prime == 0
-  end
-
-  def self.find_factors(number)
-    return [number] if Prime.prime?(number)
-
-    factor = Prime.first(7).find { |prime| is_factor?(number, prime) }
-
-    if factor
-      [factor].concat find_factors(number/factor)
-    else
-      []
-    end
+    (number % prime).zero?
   end
 
   def self.convert(number)
-    output = ''
-
-    factors = find_factors(number)
-    output << 'Pling' if factors.include? 3
-    output << 'Plang' if factors.include? 5
-    output << 'Plong' if factors.include? 7
+    output = ''.tap do |output|
+      {
+        3 => 'Pling',
+        5 => 'Plang',
+        7 => 'Plong'
+      }.each do |prime, sound|
+        output << sound if is_factor?(number, prime)
+      end
+    end
 
     output.size == 0 ? number.to_s : output
   end
