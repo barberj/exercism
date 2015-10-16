6317a8ca43f77f15ece9e0bb80bd74e2d13fc7d6
Justin Barber <barber.justin@gmail.com>
2015-10-16 15:02:21 -0400

    have default use sum method, fix bug

--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -1,22 +1,16 @@
 -module(sum_of_multiples).
 -export([sumOfMultiplesDefault/1, sumOfMultiples/2]).
 
-sumOfMultiplesDefault(_, 0) -> 0;
-sumOfMultiplesDefault(N, NaturalNumber) ->
-  if
-    NaturalNumber rem 3 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
-    NaturalNumber rem 5 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
-    true                      -> 0 + sumOfMultiplesDefault(N, NaturalNumber - 1)
-  end.
-sumOfMultiplesDefault(N) -> sumOfMultiplesDefault(N, N-1).
+sumOfMultiplesDefault(N) -> sumOfMultiples([3,5], N).
 
 sumOfMultiples(_, 0) -> 0;
 sumOfMultiples(Multiples, N) ->
+  NaturalNumber = N - 1,
   IsFactor = fun(X) -> if X =:= 0 -> true; true -> false end end,
-  MultipleIsFactor = lists:any(IsFactor, [N rem Multiple || Multiple <- Multiples]),
+  MultipleIsFactor = lists:any(IsFactor, [NaturalNumber rem Multiple || Multiple <- Multiples]),
   if
     MultipleIsFactor =:= true ->
-      N + sumOfMultiples(Multiples, N-1);
+      NaturalNumber + sumOfMultiples(Multiples, NaturalNumber);
     true       ->
-      0 + sumOfMultiples(Multiples, N-1)
+      0 + sumOfMultiples(Multiples, NaturalNumber)
   end.
