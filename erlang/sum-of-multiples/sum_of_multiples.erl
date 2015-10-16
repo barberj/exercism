292638f4db56e835d50fd52596b0cabb1a7415ba
Justin Barber <barber.justin@gmail.com>
2015-10-16 11:45:27 -0400

    i miss understood the instructions. 3 specs green

--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -4,12 +4,9 @@
 sumOfMultiplesDefault(_, 0) -> 0;
 sumOfMultiplesDefault(N, NaturalNumber) ->
   if
-    N rem NaturalNumber =:= 0 ->
-      io:fwrite("~w is a factor~n", [NaturalNumber]),
-      NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
-    true                      ->
-      io:fwrite("~w is not a factor~n", [NaturalNumber]),
-      0 + sumOfMultiplesDefault(N, NaturalNumber - 1)
+    NaturalNumber rem 3 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
+    NaturalNumber rem 5 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
+    true          -> 0 + sumOfMultiplesDefault(N, NaturalNumber - 1)
   end.
 sumOfMultiplesDefault(N) -> sumOfMultiplesDefault(N, N-1).
 sumOfMultiples(_, N) -> N.
