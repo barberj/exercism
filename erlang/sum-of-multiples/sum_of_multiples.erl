23c7a6c5c0b63ebd7516dc07cb14ba13ed2e982e
Justin Barber <barber.justin@gmail.com>
2015-10-16 14:52:48 -0400

    specs green, time to refactor

--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -6,7 +6,17 @@ sumOfMultiplesDefault(N, NaturalNumber) ->
   if
     NaturalNumber rem 3 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
     NaturalNumber rem 5 =:= 0 -> NaturalNumber + sumOfMultiplesDefault(N, NaturalNumber - 1);
-    true          -> 0 + sumOfMultiplesDefault(N, NaturalNumber - 1)
+    true                      -> 0 + sumOfMultiplesDefault(N, NaturalNumber - 1)
   end.
 sumOfMultiplesDefault(N) -> sumOfMultiplesDefault(N, N-1).
-sumOfMultiples(_, N) -> N.
+
+sumOfMultiples(_, 0) -> 0;
+sumOfMultiples(Multiples, N) ->
+  Big = fun(X) -> if X =:= 0 -> true; true -> false end end,
+  J = lists:any(Big, [N rem Multiple || Multiple <- Multiples]),
+  if
+    J =:= true ->
+      N + sumOfMultiples(Multiples, N-1);
+    true       ->
+      0 + sumOfMultiples(Multiples, N-1)
+  end.
