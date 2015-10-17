81cd3f9df3e74bc34baa42a0a8a98908b17a9d92
Justin Barber <barber.justin@gmail.com>
2015-10-17 07:28:24 -0400

    add specs

--- erlang/strain/strain.erl
+++ erlang/strain/strain.erl
@@ -1,5 +1,8 @@
 -module(strain).
 -export([keep/2,discard/2]).
 
+-spec keep(fun((term()) -> boolean()), list()) -> list().
 keep(Pred, Ls) -> [X || X <- Ls, Pred(X)].
+
+-spec discard(fun((term()) -> boolean()), list()) -> list().
 discard(Pred, Ls) -> [X || X <- Ls, not Pred(X)].
