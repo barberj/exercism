62c5e9d2e23bff899a4f771d668a194f9ef5d674
Justin Barber <barber.justin@gmail.com>
2015-10-20 07:29:02 -0400

    specs green

--- erlang/anagram/anagram.erl
+++ erlang/anagram/anagram.erl
@@ -4,12 +4,13 @@
 is_anagram(W,W) -> false;
 is_anagram(W,C) -> lists:sum(W) == lists:sum(C).
 
--spec(find(list(), list()) -> list()).
-find(_,[],Acc) -> Acc;
+find(_,[],Acc)          -> lists:reverse(Acc);
 find(Word,[Word|T],Acc) -> find(Word,T,Acc);
-find(Word,[H|T],Acc) ->
+find(Word,[H|T],Acc)    ->
   case is_anagram(string:to_lower(Word), string:to_lower(H)) of
     true  -> find(Word, T, [H|Acc]);
     false -> find(Word, T, Acc)
   end.
+
+-spec(find(list(), list()) -> list()).
 find(Word,Possible) -> find(Word,Possible,[]).
