ab7f610c4999a38c6e6c4884d610c1e180990a76
Justin Barber <barber.justin@gmail.com>
2015-10-20 07:20:27 -0400

    8 of 9 passing. failing is due to returned list being reversed

--- erlang/anagram/anagram.erl
+++ erlang/anagram/anagram.erl
@@ -1,13 +1,15 @@
 -module(anagram).
 -export([find/2]).
 
+is_anagram(W,W) -> false;
+is_anagram(W,C) -> lists:sum(W) == lists:sum(C).
+
 -spec(find(list(), list()) -> list()).
 find(_,[],Acc) -> Acc;
 find(Word,[Word|T],Acc) -> find(Word,T,Acc);
 find(Word,[H|T],Acc) ->
-  case lists:sum(Word) == lists:sum(H) of
+  case is_anagram(string:to_lower(Word), string:to_lower(H)) of
     true  -> find(Word, T, [H|Acc]);
     false -> find(Word, T, Acc)
   end.
 find(Word,Possible) -> find(Word,Possible,[]).
-
