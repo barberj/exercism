a0c94931dc06135538d612b4527837c5c052deb8
Justin Barber <barber.justin@gmail.com>
2015-10-19 21:08:24 -0400

    lean on pattern matching

--- erlang/point-mutations/dna.erl
+++ erlang/point-mutations/dna.erl
@@ -2,11 +2,8 @@
 -export([hamming_distance/2]).
 
 hamming_distance(A, A, Acc)           -> Acc;
-hamming_distance([A|AT], [B|BT], Acc) ->
-  if
-    A == B -> hamming_distance(AT, BT, Acc);
-    true   -> hamming_distance(AT, BT, Acc + 1)
-  end.
+hamming_distance([A|AT], [A|BT], Acc) -> hamming_distance(AT, BT, Acc);
+hamming_distance([A|AT], [B|BT], Acc) -> hamming_distance(AT, BT, Acc + 1).
 
 -spec(hamming_distance(list(), list()) -> integer()).
 hamming_distance(A, B) -> hamming_distance(A, B, 0).
