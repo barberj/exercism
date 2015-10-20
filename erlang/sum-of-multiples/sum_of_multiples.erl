a78498a1868d370963d55c18f7e182ccedc4c40a
Justin Barber <barber.justin@gmail.com>
2015-10-20 17:01:39 -0400

    guard against negative numbers in sum of multiples

--- erlang/sum-of-multiples/sum_of_multiples.erl
+++ erlang/sum-of-multiples/sum_of_multiples.erl
@@ -4,7 +4,7 @@
 sumOfMultiplesDefault(N) -> sumOfMultiples([3,5], N).
 
 sumOfMultiples(_, 0) -> 0;
-sumOfMultiples(Multiples, N) ->
+sumOfMultiples(Multiples, N) when N > 0 ->
   NaturalNumber = N - 1,
   case lists:any(fun(Multiple) -> NaturalNumber rem Multiple == 0 end, Multiples) of
     true  -> NaturalNumber + sumOfMultiples(Multiples, NaturalNumber);
