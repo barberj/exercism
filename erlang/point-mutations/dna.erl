a67064e6fb7eddc740b0c9cbff7f02f44b70b7ea
Justin Barber <barber.justin@gmail.com>
2015-10-19 21:11:03 -0400

    remove unused warning

--- erlang/point-mutations/dna.erl
+++ erlang/point-mutations/dna.erl
@@ -3,7 +3,7 @@
 
 hamming_distance(A, A, Acc)           -> Acc;
 hamming_distance([A|AT], [A|BT], Acc) -> hamming_distance(AT, BT, Acc);
-hamming_distance([A|AT], [B|BT], Acc) -> hamming_distance(AT, BT, Acc + 1).
+hamming_distance([_|AT], [_|BT], Acc) -> hamming_distance(AT, BT, Acc + 1).
 
 -spec(hamming_distance(list(), list()) -> integer()).
 hamming_distance(A, B) -> hamming_distance(A, B, 0).
