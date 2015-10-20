5f469d7792902f0b988fc78f4e2c5749f75cdc6d
Justin Barber <barber.justin@gmail.com>
2015-10-20 06:56:31 -0400

    6 of 9 anagram passing. Still need to handle capitals

--- erlang/anagram/anagram.erl
+++ erlang/anagram/anagram.erl
@@ -3,6 +3,7 @@
 
 -spec(find(list(), list()) -> list()).
 find(_,[],Acc) -> Acc;
+find(Word,[Word|T],Acc) -> find(Word,T,Acc);
 find(Word,[H|T],Acc) ->
   case lists:sum(Word) == lists:sum(H) of
     true  -> find(Word, T, [H|Acc]);
