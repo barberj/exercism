d621b889d6b262e4a066de7eb445d64de8f68d80
Justin Barber <barber.justin@gmail.com>
2015-10-20 06:49:53 -0400

    5 out of 4 anagram specs passing

---
+++ erlang/anagram/anagram.erl
@@ -0,0 +1,12 @@
+-module(anagram).
+-export([find/2]).
+
+-spec(find(list(), list()) -> list()).
+find(_,[],Acc) -> Acc;
+find(Word,[H|T],Acc) ->
+  case lists:sum(Word) == lists:sum(H) of
+    true  -> find(Word, T, [H|Acc]);
+    false -> find(Word, T, Acc)
+  end.
+find(Word,Possible) -> find(Word,Possible,[]).
+
