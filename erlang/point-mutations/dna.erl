f954122a928e5a2c15f46452ca58f46b494a9377
Justin Barber <barber.justin@gmail.com>
2015-10-19 21:05:50 -0400

    make tail recursive

--- erlang/point-mutations/dna.erl
+++ erlang/point-mutations/dna.erl
@@ -1,10 +1,12 @@
 -module(dna).
 -export([hamming_distance/2]).
 
--spec(hamming_distance(list(), list()) -> integer()).
-hamming_distance(A, A)           -> 0;
-hamming_distance([A|AT], [B|BT]) ->
+hamming_distance(A, A, Acc)           -> Acc;
+hamming_distance([A|AT], [B|BT], Acc) ->
   if
-    A == B -> 0 + hamming_distance(AT, BT);
-    true   -> 1 + hamming_distance(AT, BT)
+    A == B -> hamming_distance(AT, BT, Acc);
+    true   -> hamming_distance(AT, BT, Acc + 1)
   end.
+
+-spec(hamming_distance(list(), list()) -> integer()).
+hamming_distance(A, B) -> hamming_distance(A, B, 0).
