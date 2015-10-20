d5452e528edea7787f4aa3301c995a21bac52ab8
Justin Barber <barber.justin@gmail.com>
2015-10-19 22:49:56 -0400

    lean on math module

--- erlang/grains/grains.erl
+++ erlang/grains/grains.erl
@@ -1,9 +1,7 @@
 -module(grains).
 -export([square/1, total/0]).
 
-square(N, N, Acc) -> Acc;
-square(N, C, Acc) -> square(N, C+1, Acc*2).
-square(N)         -> square(N, 1, 1).
+square(N) -> trunc(math:pow(2, N-1)).
 
 total(64, Acc) -> square(64)+Acc;
 total(N, Acc)  -> total(N+1, square(N)+Acc).
