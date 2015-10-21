5dbd927c0b3172634519e6a2de1ffe4ca6339d00
Justin Barber <barber.justin@gmail.com>
2015-10-20 21:32:28 -0400

    dna:count specs green

--- erlang/nucleotide-count/dna.erl
+++ erlang/nucleotide-count/dna.erl
@@ -2,5 +2,18 @@
 -export([count/2]).
 
 -spec(count(string(), string()) -> integer()).
-count([], _Nucleotide) -> 0;
-count(_,_) -> error("Invalid nucleotide").
+
+count([], _Nucleotide, Acc)   -> Acc;
+count([$A|T], "A", Acc)       -> count(T, "A", Acc + 1);
+count([$T|T], "T", Acc)       -> count(T, "T", Acc + 1);
+count([$C|T], "C", Acc)       -> count(T, "C", Acc + 1);
+count([$G|T], "G", Acc)       -> count(T, "G", Acc + 1);
+count([_|T], Nucleotide, Acc) -> count(T, Nucleotide, Acc).
+
+count(Dna, Nucleotide) when
+  Nucleotide == "A";
+  Nucleotide == "T";
+  Nucleotide == "C";
+  Nucleotide == "G" ->
+    count(Dna, Nucleotide, 0);
+count(_,_)            -> error("Invalid nucleotide").
