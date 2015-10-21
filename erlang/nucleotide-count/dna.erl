b33d2945d23605b2a71bc4f1f9ba3b7775f3038d
Justin Barber <barber.justin@gmail.com>
2015-10-20 21:45:44 -0400

    specs green

--- erlang/nucleotide-count/dna.erl
+++ erlang/nucleotide-count/dna.erl
@@ -1,7 +1,6 @@
 -module(dna).
--export([count/2]).
+-export([count/2, nucleotide_counts/1]).
 
--spec(count(string(), string()) -> integer()).
 
 count([], _Nucleotide, Acc)   -> Acc;
 count([$A|T], "A", Acc)       -> count(T, "A", Acc + 1);
@@ -10,6 +9,7 @@ count([$C|T], "C", Acc)       -> count(T, "C", Acc + 1);
 count([$G|T], "G", Acc)       -> count(T, "G", Acc + 1);
 count([_|T], Nucleotide, Acc) -> count(T, Nucleotide, Acc).
 
+-spec(count(string(), string()) -> integer()).
 count(Dna, Nucleotide) when
   Nucleotide == "A";
   Nucleotide == "T";
@@ -17,3 +17,11 @@ count(Dna, Nucleotide) when
   Nucleotide == "G" ->
     count(Dna, Nucleotide, 0);
 count(_,_)            -> error("Invalid nucleotide").
+
+nucleotide_counts(_Dna, [], Acc) -> lists:reverse(Acc);
+nucleotide_counts(Dna, [Nucleotide|T], Acc) ->
+  nucleotide_counts(Dna, T, [{Nucleotide, count(Dna, Nucleotide)}|Acc]).
+
+-spec(nucleotide_counts(string()) -> list(tuple())).
+nucleotide_counts([]) -> [{"A", 0}, {"T", 0}, {"C", 0}, {"G", 0}];
+nucleotide_counts(Dna) -> nucleotide_counts(Dna, ["A","T","C","G"], []).
