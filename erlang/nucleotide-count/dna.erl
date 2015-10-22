7dc31d45415f47e1f88920cb4ba0642771c7e4ae
Justin Barber <barber.justin@gmail.com>
2015-10-22 18:06:56 -0400

    review updates from sebhans

--- erlang/nucleotide-count/dna.erl
+++ erlang/nucleotide-count/dna.erl
@@ -2,18 +2,15 @@
 -export([count/2, nucleotide_counts/1]).
 
 count([], _Nucleotide, Acc)   -> Acc;
-count([$A|T], "A", Acc)       -> count(T, "A", Acc + 1);
-count([$T|T], "T", Acc)       -> count(T, "T", Acc + 1);
-count([$C|T], "C", Acc)       -> count(T, "C", Acc + 1);
-count([$G|T], "G", Acc)       -> count(T, "G", Acc + 1);
+count([N|T], N, Acc)          -> count(T, N, Acc + 1);
 count([_|T], Nucleotide, Acc) -> count(T, Nucleotide, Acc).
 
 -spec(count(string(), string()) -> integer()).
-count(Dna, Nucleotide) when
-  Nucleotide == "A";
-  Nucleotide == "T";
-  Nucleotide == "C";
-  Nucleotide == "G" ->
+count(Dna, [Nucleotide]) when
+  Nucleotide == $A;
+  Nucleotide == $T;
+  Nucleotide == $C;
+  Nucleotide == $G ->
     count(Dna, Nucleotide, 0);
 count(_,_)          -> error("Invalid nucleotide").
 
