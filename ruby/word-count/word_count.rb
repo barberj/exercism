4746e9e334f6b556b8087441576c926eabf07bdc
Justin Barber <barber.justin@gmail.com>
2015-10-25 03:07:28 -0400

    2 failing, better abstraction

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -6,17 +6,25 @@ class Phrase
     @phrase = phrase
   end
 
-  def word_count
-    @word_count ||= begin
-      @words = phrase.split.map { |part| (part[/\w+/] || "").downcase }
-      @words.reduce({}) do |count, word|
-        if !word.empty?
-          count[word] ||= 0
-          count[word] += 1
-        end
-        count
+  def words
+    @words ||= phrase.split(%r{[,\s*]}).map do |part|
+      normed = (part[/\w+/] || "").downcase
+      normed.empty? ? nil : normed
+    end.compact
+  end
+
+  def reduce_words
+    @reduced ||= words.reduce({}) do |count, word|
+      if !word.empty?
+        count[word] ||= 0
+        count[word] += 1
       end
+      count
     end
+  end
+
+  def word_count
+    @word_count ||= reduce_words
   end
 
   private
