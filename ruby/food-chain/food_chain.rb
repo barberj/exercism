50c56a318f02ee3c46d59f5050fe51728e357bd6
Justin Barber <barber.justin@gmail.com>
2015-11-12 18:35:40 -0500

    specs green

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -13,5 +13,28 @@ class FoodChain
   }
 
   def self.song
+    swallowed = []
+    aggregate = []
+
+    FOOD.each do |animal, verse|
+      aggregate << "I know an old lady who swallowed a #{animal}."
+      aggregate << verse unless swallowed.empty?
+      last_eaten = animal
+
+      break if FOOD.length - 1 == swallowed.length
+
+      swallowed.each do |eaten|
+        catch_phrase = "She swallowed the #{last_eaten} to catch the #{eaten}."
+        catch_phrase = "#{catch_phrase[0...-1]} that #{FOOD[eaten][3..-1]}" if eaten == swallowed[-2]
+
+        aggregate << catch_phrase
+        last_eaten = eaten
+      end
+      swallowed.unshift(animal)
+
+      aggregate << "#{FOOD[last_eaten]}\n"
+    end
+
+    aggregate.join("\n") + "\n"
   end
 end
