27c7694d964297a5bea5a45357713b9ecf18fc82
Justin Barber <barber.justin@gmail.com>
2015-11-12 15:35:40 -0500

    food constant

--- ruby/food-chain/food_chain.rb
+++ ruby/food-chain/food_chain.rb
@@ -1,6 +1,17 @@
 class FoodChain
   VERSION = 2
 
+  FOOD = {
+    fly: "I don't know why she swallowed the fly. Perhaps she'll die.",
+    spider: "It wriggled and jiggled and tickled inside her.",
+    bird: "How absurd to swallow a bird!",
+    cat: "Imagine that, to swallow a cat!",
+    dog: "What a hog, to swallow a dog!",
+    goat: "Just opened her throat and swallowed a goat!",
+    cow: "I don't know how she swallowed a cow!",
+    horse: "She's dead, of course!"
+  }
+
   def self.song
   end
 end
