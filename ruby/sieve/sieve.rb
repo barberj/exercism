5101d7dc718a40655c7180c665c2fe8a8c1e4422
Justin Barber <barber.justin@gmail.com>
2015-11-17 15:16:55 -0500

    Sieve green

--- ruby/sieve/sieve.rb
+++ ruby/sieve/sieve.rb
@@ -1,2 +1,22 @@
 class Sieve
+  attr_accessor :limit
+  attr_accessor :numbers
+  attr_accessor :marked
+
+  def initialize(given_limit)
+    @limit = given_limit
+    @numbers = 2.upto(limit).to_a
+    @marked = []
+  end
+
+  def primes
+    2.upto(limit).each do |n|
+      numbers.each do |number|
+        next if marked.include?(number)
+        marked << number if number != n && number % n == 0
+      end
+    end
+
+    numbers - marked
+  end
 end
