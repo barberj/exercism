e21547fa7f79dcc70aa67af6dded0ac7bf911a72
Justin Barber <barber.justin@gmail.com>
2015-11-23 09:55:07 -0500

    use processes for state change

--- erlang/robot-simulator/robot_simulator.erl
+++ erlang/robot-simulator/robot_simulator.erl
@@ -1,13 +1,42 @@
 -module(robot_simulator).
--export([create/0, direction/1, position/1, place/3]).
+-export([create/0, direction/1, position/1, place/3, simulate_robot/1]).
 %-export([create/0,direction/1,position/1,place/3,left/1,right/1,control/2]).
 
 -record(robot, {direction=undefined, position={undefined, undefined}}).
 
-create() -> #robot{}.
+create() ->
+  spawn(?MODULE, simulate_robot, [#robot{}]).
 
-direction(#robot{direction=Direction}) -> Direction.
-position(#robot{position=Position}) -> Position.
+direction(Robot) ->
+  Robot ! {self(), {direction}},
+  receive
+    {Robot, Direction} -> Direction
+  end.
+
+position(Robot) ->
+  Robot ! {self(), {position}},
+  receive
+    {Robot, Position} -> Position
+  end.
 
 place(Robot, Direction, Position) ->
-  #robot{direction=Direction, position=Position}.
+  Robot ! {self(), {Direction, Position}},
+  receive
+    {Robot, ok} -> ok
+  end.
+
+simulate_robot(Robot) ->
+  receive
+    {From, {hi}} ->
+      From ! {self(), yello};
+    {From, {Direction, Position}} ->
+      NewRobot = #robot{direction=Direction, position=Position},
+      From ! {self(), ok},
+      simulate_robot(NewRobot);
+    {From, {direction}} ->
+      From ! {self(), Robot#robot.direction},
+      simulate_robot(Robot);
+    {From, {position}} ->
+      From ! {self(), Robot#robot.position},
+      simulate_robot(Robot)
+  end.
