7231987bbd3d095ae0d0b2390ca515b9d98cfb7f
Justin Barber <barber.justin@gmail.com>
2015-11-24 22:20:51 -0500

    style updatesg

--- erlang/binary/binary_string.erl
+++ erlang/binary/binary_string.erl
@@ -4,10 +4,7 @@
 to_decimal(String) ->
   to_decimal(lists:reverse(String), 0, 0).
 
-to_decimal([], Acc, _)     ->
-  round(Acc);
-to_decimal([$0|T], Acc, I) ->
-  to_decimal(T, Acc, I + 1);
-to_decimal([$1|T], Acc, I) ->
-  to_decimal(T, Acc + (1 * math:pow(2, I)), I + 1);
+to_decimal([], Acc, _)     -> round(Acc);
+to_decimal([$0|T], Acc, I) -> to_decimal(T, Acc, I + 1);
+to_decimal([$1|T], Acc, I) -> to_decimal(T, Acc + (1 * math:pow(2, I)), I + 1);
 to_decimal([_|T], Acc, I)  -> to_decimal(T, Acc, I + 1).
