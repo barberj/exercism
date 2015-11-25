4c46342fcf5fca9317594b308f5605826124a609
Justin Barber <barber.justin@gmail.com>
2015-11-24 22:13:30 -0500

    erlang binary_string

---
+++ erlang/binary/binary_string.erl
@@ -0,0 +1,13 @@
+-module(binary_string).
+-export([to_decimal/1]).
+
+to_decimal(String) ->
+  to_decimal(lists:reverse(String), 0, 0).
+
+to_decimal([], Acc, _)     ->
+  round(Acc);
+to_decimal([$0|T], Acc, I) ->
+  to_decimal(T, Acc, I + 1);
+to_decimal([$1|T], Acc, I) ->
+  to_decimal(T, Acc + (1 * math:pow(2, I)), I + 1);
+to_decimal([_|T], Acc, I)  -> to_decimal(T, Acc, I + 1).
