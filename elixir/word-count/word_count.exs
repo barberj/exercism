77a089672e2870176969062b9bf7c52e6c9a7544
Justin Barber <barber.justin@gmail.com>
2015-11-26 08:54:25 -0500

    hrm... some immutable things held over from erlang

--- elixir/word-count/word_count.exs
+++ elixir/word-count/word_count.exs
@@ -6,6 +6,16 @@ defmodule Words do
   """
   @spec count(String.t) :: map()
   def count(sentence) do
+    map = %{}
+    Enum.each(tokenize(sentence), fn word -> value = map[word]; Dict.put(map, word, value || 0 + 1) end)
 
+    map
+  end
+
+  defp tokenize(sentence) do
+    cleaned = Regex.replace(~r/[^A-Za-z0-9]+/, sentence, " ")
+    stripped = String.strip(cleaned)
+    normed = String.downcase(stripped)
+    Regex.split(~r/[^a-z0-9]+/, normed)
   end
 end
