b7d987a9d09f2016c7398c53ee803bc009baa505
Justin Barber <barber.justin@gmail.com>
2015-11-25 21:13:28 -0500

    re:split is a better tokenizer

--- erlang/word-count/word_count.erl
+++ erlang/word-count/word_count.erl
@@ -3,4 +3,4 @@
 
 count([], Map) -> dict:from_list(maps:to_list(Map));
 count([W|T], Map) -> count(T, maps:put(W, maps:get(W, Map, 0) + 1, Map)).
-count(String) -> count(string:tokens(String, " "), #{}).
+count(String) -> count(re:split(String,"[^A-Za-z]+", [trim,{return, list}]), #{}).
