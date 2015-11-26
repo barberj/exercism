bbfb1a368fba7f2d7ffbc1d9e02a5878612cab35
Justin Barber <barber.justin@gmail.com>
2015-11-25 21:47:56 -0500

    spec passing

--- erlang/word-count/word_count.erl
+++ erlang/word-count/word_count.erl
@@ -1,6 +1,13 @@
 -module(word_count).
 -export([count/1]).
 
-count([], Map) -> dict:from_list(maps:to_list(Map));
-count([W|T], Map) -> count(T, maps:put(W, maps:get(W, Map, 0) + 1, Map)).
-count(String) -> count(re:split(String,"[^A-Za-z]+", [trim,{return, list}]), #{}).
+count([], Map) ->
+  dict:from_list(maps:to_list(Map));
+count([W|T], Map) ->
+  count(T, maps:put(W, maps:get(W, Map, 0) + 1, Map)).
+count(String) ->
+  Cleaned = re:replace(String,"[^A-Za-z0-9]+", " ", [{return, list}]),
+  Stripped = string:strip(Cleaned),
+  Normed = string:to_lower(Stripped),
+  Tokens = re:split(Normed, "[^a-z0-9]+", [trim,{return, list}]),
+  count(Tokens, #{}).
