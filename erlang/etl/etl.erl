d31b4275b9498203e9ebbe7929126e261d1837da
Justin Barber <barber.justin@gmail.com>
2015-12-04 14:21:01 -0500

    style updates

--- erlang/etl/etl.erl
+++ erlang/etl/etl.erl
@@ -1,14 +1,13 @@
 -module(etl).
 -export([transform/1]).
 
-map_letters(_, [], Acc)            -> Acc;
+map_letters(_, [], Acc)                -> Acc;
 map_letters(Score, [Letter|Tail], Acc) ->
   map_letters(Score, Tail, Acc ++ [{string:to_lower(Letter), Score}]).
 
-
-
-transform([], Acc) -> Acc;
+transform([], Acc)                     -> Acc;
 transform([{Score,Letters}|Tail], Acc) ->
   LetterMap = map_letters(Score, Letters, []),
   transform(Tail, Acc ++ LetterMap).
+
 transform(ScrabbleData) -> transform(ScrabbleData, []).
