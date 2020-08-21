30f825e556498c91dfc23f952fedca9984693779
Justin Barber <barber.justin@gmail.com>
2020-08-21 10:15:35 -0400

    resistor_color_duo

---
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -0,0 +1,22 @@
+class ResistorColorDuo
+  COLOR_MAP = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }.freeze
+
+  COLORS = COLOR_MAP.keys.freeze
+
+  def self.value(colors)
+    colors.reduce("") do |value, color|
+      "#{value}#{COLOR_MAP[color.downcase]}"
+    end.to_i
+  end
+end
