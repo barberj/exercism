abacde08e295f71543a18e5c6eb0a724ae18dbc7
Justin Barber <barber.justin@gmail.com>
2020-08-20 22:11:48 -0400

    resistor

---
+++ ruby/resistor-color/resistor_color.rb
@@ -0,0 +1,29 @@
+=begin
+Write your code for the 'Resistor Color' exercise in this file. Make the tests in
+`resistor_color_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/resistor-color` directory.
+=end
+
+class ResistorColor
+  COLOR_MAP = {
+    "black" => 0,
+    "brown" => 1,
+    "red" => 2,
+    "orange" => 3,
+    "yellow" => 4,
+    "green" => 5,
+    "blue" => 6,
+    "violet" => 7,
+    "grey" => 8,
+    "white" => 9
+  }.freeze
+
+  COLORS = COLOR_MAP.keys.freeze
+
+  def self.color_code(color)
+    COLOR_MAP.fetch(color.downcase)
+  end
+end
+
